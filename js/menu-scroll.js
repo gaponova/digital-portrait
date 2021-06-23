bac9252b35c09c0ddac5e275549fd73460e43d89
gaponova <dzensmart@gmail.com>
2021-06-23 22:48:50 +0300

    Сделали секцию level со слайдером

--- js/menu-scroll.js
+++ js/menu-scroll.js
@@ -2,9 +2,9 @@ window.onscroll = () => {
   const headerbar = document.querySelector('.header');
   const Y = window.scrollY;
 
-  if (Y > 100) {
+  if (Y > 50) {
     headerbar.classList.add('bar-fixed');
-  }else if (Y < 100) {
+  }else if (Y < 50) {
     headerbar.classList.remove('bar-fixed');
   }
 };
---
+++ js/sliders.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(".level-owl-carousel").owlCarousel({
+    stagePadding: 100,
+    items: 4,
+    margin: 60,
+    loop: true,
+    autoWidth: true,
+    stagePadding: 100,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    navText: ["<img src='img/arrow-left.svg'>", "<img src='img/arrow-right.svg'>"],
+    dots: false
+
+    
+    
+  });
+});
