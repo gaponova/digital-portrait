e8452e08ed8e35b35faadd72bb05f31b4e7f93ba
gaponova <dzensmart@gmail.com>
2021-08-19 19:01:04 +0300

    Сделали слайдер для Глава 1

--- js/sliders.js
+++ js/sliders.js
@@ -23,4 +23,21 @@ $(document).ready(function () {
     navText: ["<img src='img/arrow-left.svg'>", "<img src='img/arrow-right.svg'>"],
     dots: true,
   });
+
+  $(".homework1-owl-carousel").owlCarousel({
+    stagePadding: 100,
+    items: 4,
+    margin: 60,
+    loop: true,
+    autoWidth: true,
+    autoHeight: true,
+    stagePadding: 100,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    navText: ["<img src='img/arrow-left.svg'>", "<img src='img/arrow-right.svg'>"],
+    dots: true,
+    center: true
+  });
+  
 });
