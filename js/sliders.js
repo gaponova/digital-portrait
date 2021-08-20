330610b72681008c40c4da79cfaac781ebc40187
gaponova <dzensmart@gmail.com>
2021-08-20 21:56:18 +0300

    Пофиксили аккордеон

--- js/sliders.js
+++ js/sliders.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   $(".level-owl-carousel").owlCarousel({
-    stagePadding: 100,
     items: 4,
     margin: 60,
     loop: true,
@@ -25,18 +24,16 @@ $(document).ready(function () {
   });
 
   $(".homework1-owl-carousel").owlCarousel({
-    stagePadding: 100,
     items: 4,
     margin: 60,
     loop: true,
     autoWidth: true,
-    autoHeight: true,
     stagePadding: 100,
     nav: true,
     autoplay: true,
     autoplayTimeout: 5000,
     navText: ["<img src='img/arrow-left.svg'>", "<img src='img/arrow-right.svg'>"],
-    dots: true,
+    dots: false,
     center: true
   });
   
